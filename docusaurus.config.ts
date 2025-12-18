1f5ed2e5d2e0418980810a5e3a7b0193cff41b93
Kyle Steinfeld <146304315+Pseudotools@users.noreply.github.com>
2025-12-18 14:34:59 -0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://Pseudotools.github.io',
+  url: 'https://pseudotools.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/' 
-  baseUrl: "/pseudorandom-docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Pseudotools', // Usually your GitHub org/user name.
-  projectName: 'pseudorandom-docs', // Usually your repo name.
+  projectName: 'Pseudotools Documentation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   trailingSlash: false,
