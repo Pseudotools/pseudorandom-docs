06de11e092ad47d4b6e224183f5961946b65ae83
Kyle Steinfeld <146304315+Pseudotools@users.noreply.github.com>
2025-12-18 14:27:32 -0800

    deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,17 +15,18 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Pseudotools.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // For GitHub pages deployment, it is often '/<projectName>/' 
+  baseUrl: "/pseudorandom-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Pseudotools', // Usually your GitHub org/user name.
+  projectName: 'pseudorandom-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  trailingSlash: false,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
